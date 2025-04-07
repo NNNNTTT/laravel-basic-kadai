29a562137da2dd69b7c5b749dab7b89f58b4bd70
坪田直樹 <tsubotanaoki@tsubotanaokinoMacBook-Pro.local>
2025-04-07 17:37:56 +0900

    5回目のコミット

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,10 +6,17 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\DB;
 
+use App\Models\Post;
+
 class PostController extends Controller
 {
     public function index() {
         $posts = DB::table('posts')->get();
         return view('posts.index', compact('posts'));
+    }
+
+    public function show($id) {
+        $post = Post::find($id);
+        return view('posts.show', compact('post'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ Route::get('/', function () {
 Route::get('/hello', [HelloController::class, 'index']);
 
 Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/posts/{id}', [PostController::class, 'show']);
